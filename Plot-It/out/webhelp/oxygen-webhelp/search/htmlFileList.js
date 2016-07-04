b10e0f19c22083f0811f79af41747785fa72473a
Mihaela Ilovan <ilovan@ualberta.ca>
2016-07-04 09:25:38 -0600

    added plot-it manually

---
+++ Plot-It/out/webhelp/oxygen-webhelp/search/htmlFileList.js
@@ -0,0 +1,20 @@
+//List of files which are indexed.
+fl = new Array();
+fl["0"]= "ChangeViewMode.html";
+fl["1"]= "Events.html";
+fl["2"]= "Filter Date by event range.html";
+fl["3"]= "Filter by "Event type".html";
+fl["4"]= "FilterByCountry.html";
+fl["5"]= "HidePanel.html";
+fl["6"]= "HideTimeline.html";
+fl["7"]= "Introduction to PlotIt.html";
+fl["8"]= "Panel.html";
+fl["9"]= "Reset Filters.html";
+fl["10"]= "Search Plot-It.html";
+fl["11"]= "SearchReference.html";
+fl["12"]= "Timeline.html";
+fl["13"]= "View events not in the map.html";
+fl["14"]= "View events.html";
+fl["15"]= "Working with the map.html";
+fl["16"]= "Working with the timeline.html";
+var doStem = false
