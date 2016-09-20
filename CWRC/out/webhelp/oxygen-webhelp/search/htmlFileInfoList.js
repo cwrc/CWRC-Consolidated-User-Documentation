aec9d80c0c4054bc96b05a2bef46d677a77ff77d
Megan Sellmer <sellmer@ualberta.ca>
2016-09-19 23:28:15 -0700

    CWRC documentation
    “About” reference page and profiling attributes

---
+++ CWRC/out/webhelp/oxygen-webhelp/search/htmlFileInfoList.js
@@ -0,0 +1,2 @@
+fil = new Array();
+fil["0"]= "AboutCWRC.html@@@About CWRC@@@CWRC is an online research environment that brings together scholarly materials and digital tools...";
---
+++ CWRC/out/webhelp/oxygen-webhelp/search/htmlFileList.js
@@ -0,0 +1,4 @@
+//List of files which are indexed.
+fl = new Array();
+fl["0"]= "AboutCWRC.html";
+var doStem = false
